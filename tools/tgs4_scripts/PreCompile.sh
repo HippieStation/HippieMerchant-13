66ec82fa1d477209c317a43ae64cbfc4bed2172e
Pyko <r.varpela@gmail.com>
2022-10-06 19:22:39 +0300

    Update PreCompile.sh

--- tools/tgs4_scripts/PreCompile.sh
+++ tools/tgs4_scripts/PreCompile.sh
@@ -18,7 +18,7 @@ has_git="$(command -v git)"
 has_cargo="$(command -v ~/.cargo/bin/cargo)"
 has_sudo="$(command -v sudo)"
 has_grep="$(command -v grep)"
-has_ytdlp="$(command -v yt-dlp)"
+has_youtubedl="$(command -v youtube-dl)"
 has_pip3="$(command -v pip3)"
 set -e
 
@@ -29,18 +29,28 @@ if ! [ -x "$has_cargo" ]; then
 	. ~/.profile
 fi
 
-# dnf packages, libssl needed by rust-g but not included in TGS barebones install
-echo "Installing dependencies..."
-if [ -x "$has_sudo" ]; then
-	sudo dnf install -y git openssl-devel openssl-devel.i686 gcc libstdc++.i686 glibc-devel.i686 zlib-devel.i686 pkgconf-pkg-config
-else
-	dnf install -y git openssl-devel openssl-devel.i686 gcc libstdc++.i686 glibc-devel.i686 zlib-devel.i686 pkgconf-pkg-config
+# apt packages, libssl needed by rust-g but not included in TGS barebones install
+if ! ( [ -x "$has_git" ] && [ -x "$has_grep" ] && [ -f "/usr/lib/i386-linux-gnu/libssl.so" ] ); then
+	echo "Installing apt dependencies..."
+	if ! [ -x "$has_sudo" ]; then
+		dpkg --add-architecture i386
+		apt-get update
+		apt-get install -y git libssl-dev:i386
+		rm -rf /var/lib/apt/lists/*
+	else
+		sudo dpkg --add-architecture i386
+		sudo apt-get update
+		sudo apt-get install -y git libssl-dev:i386
+		sudo rm -rf /var/lib/apt/lists/*
+	fi
 fi
-
+dpkg --add-architecture i386
+apt-get update
+apt-get install -y lib32z1 pkg-config libssl-dev:i386 libssl-dev libssl1.1:i386
 # update rust-g
 if [ ! -d "rust-g" ]; then
 	echo "Cloning rust-g..."
-	git clone https://github.com/The-Merchants-Guild/rust-g
+	git clone https://github.com/tgstation/rust-g
 	cd rust-g
 	~/.cargo/bin/rustup target add i686-unknown-linux-gnu
 else
@@ -56,24 +66,19 @@ env PKG_CONFIG_ALLOW_CROSS=1 ~/.cargo/bin/cargo build --release --target=i686-un
 mv target/i686-unknown-linux-gnu/release/librust_g.so "$1/librust_g.so"
 cd ..
 
-# install or update yt-dlp when not present, or if it is present with pip3,
+# install or update youtube-dl when not present, or if it is present with pip3,
 # which we assume was used to install it
-if ! [ -x "$has_ytdlp" ]; then
-	echo "Installing yt-dlp with pip3..."
-	if [ -x "$has_sudo" ]; then
-		sudo dnf install -y python3-pip
-		sudo pip3 install yt-dlp
+if ! [ -x "$has_youtubedl" ]; then
+	echo "Installing youtube-dl with pip3..."
+	if ! [ -x "$has_sudo" ]; then
+		apt-get install -y python3 python3-pip
 	else
-		dnf install -y python3-pip
-		pip3 install yt-dlp
+		sudo apt-get install -y python3 python3-pip
 	fi
+	pip3 install youtube-dl
 elif [ -x "$has_pip3" ]; then
-	echo "Ensuring yt-dlp is up-to-date with pip3..."
-	if [ -x "$has_sudo" ]; then
-		sudo pip3 install yt-dlp -U
-	else
-		pip3 install yt-dlp -U
-	fi
+	echo "Ensuring youtube-dl is up-to-date with pip3..."
+	pip3 install youtube-dl -U
 fi
 
 # compile tgui
