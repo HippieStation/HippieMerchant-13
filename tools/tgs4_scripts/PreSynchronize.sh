20d66eafb779f102a9212c77c8ba1cccc88bfbb7
Pyko <r.varpela@gmail.com>
2022-10-06 19:23:24 +0300

    Update PreSynchronize.sh

--- tools/tgs4_scripts/PreSynchronize.sh
+++ tools/tgs4_scripts/PreSynchronize.sh
@@ -7,21 +7,21 @@ has_pip="$(command -v pip3)"
 
 set -e
 
-if ! { [ -x "$has_python" ] && [ -x "$has_pip" ] && [ -x "$has_git" ]; }; then
-    echo "Installing dependencies..."
-    if [ -x "$has_sudo" ]; then
-        sudo dnf install -y python3-pip git
+if ! { [ -x "$has_python" ] && [ -x "$has_pip" ] && [ -x "$has_git" ];  }; then
+    echo "Installing apt dependencies..."
+    if ! [ -x "$has_sudo" ]; then
+        apt update
+        apt install -y python3 python3-pip git
+		rm -rf /var/lib/apt/lists/*
     else
-        dnf install -y python3-pip git
+        sudo apt update
+        sudo apt install -y python3 python3-pip git
+		sudo rm -rf /var/lib/apt/lists/*
     fi
 fi
 
 echo "Installing pip dependencies..."
-if [ -x "$has_sudo" ]; then
-	sudo pip3 install PyYaml beautifulsoup4
-else
-	pip3 install PyYaml beautifulsoup4
-fi
+pip3 install PyYaml beautifulsoup4
 
 cd $1
 
