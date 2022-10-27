HolyTigerGod/Voidcrew

Micro-optimize GetIdFromArguments to be 48% faster, gaining 0.48s of init time on local (likely more in prod) (#69659)
About The Pull Request

    Avoids stringifying key unless its necessary. This was done redundantly twice, but I locked it to just the isnum path, as REF will always return a string, and the other path passes istext.
    Use sortTim directly instead of sort_list. sort_list is just sortTim but it copies the list, so it's just wasted cost.

I still would like the bespoke element key option, as that's the only way to drastically cut down costs on things like item descriptions and decals, but this is good for the general use case, and makes it marginally less pressing.

I also want to test if we'd be better off inserting into the list in sorted order rather than sorting it all in the end, but I suspect not.

code/modules/unit_tests/dcs_get_id_from_elements.dm
@@ -1,0 +1,46 @@
+/// Tests that DCS' GetIdFromArguments works as expected with standard and odd cases
+/datum/unit_test/dcs_get_id_from_arguments
+
+/datum/unit_test/dcs_get_id_from_arguments/Run()
+	assert_equal(list(1), list(1))
+	assert_equal(list(1, 2), list(1, 2))
+	assert_equal(list(src), list(src))
+
+	assert_equal(
+		list(a = "x", b = "y", c = "z"),
+		list(b = "y", a = "x", c = "z"),
+		list(c = "z", a = "x", b = "y"),
+	)
+
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list(1, 2)), get_id_from_arguments(list(2, 1)), "Swapped arguments should not return the same id")
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list(1, a = "x")), get_id_from_arguments(list(1)), "Named arguments were ignored when creating ids")
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list(1, a = "x")), get_id_from_arguments(list(a = "x")), "Unnamed arguments were ignored when creating ids")
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list(src)), get_id_from_arguments(list(world)), "References to different datums should not return the same id")
+
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list()), SSdcs.GetIdFromArguments(list(/datum/element/dcs_get_id_from_arguments_mock_element2)), "Different elements should not match the same id")
+
+/datum/unit_test/dcs_get_id_from_arguments/proc/assert_equal(reference, ...)
+	var/result = get_id_from_arguments(reference)
+
+	// Start at 1 so the 2nd argument is 2
+	var/index = 1
+
+	for (var/other_case in args)
+		index += 1
+
+		var/other_result = get_id_from_arguments(other_case)
+
+		if (other_result == result)
+			continue
+
+		TEST_FAIL("Case #[index] produces a different GetIdFromArguments result from the first. [other_result] != [result]")
+
+/datum/unit_test/dcs_get_id_from_arguments/proc/get_id_from_arguments(list/arguments)
+	return SSdcs.GetIdFromArguments(list(/datum/element/dcs_get_id_from_arguments_mock_element) + arguments)
+
+// Necessary because GetIdFromArguments uses id_arg_index from an element type
+/datum/element/dcs_get_id_from_arguments_mock_element
+	id_arg_index = 2
+
+/datum/element/dcs_get_id_from_arguments_mock_element2
+	id_arg_index = 2

HippieStation/HippieMerchant-13

code/modules/unit_tests/dcs_get_id_from_elements.dm
@@ -1,0 +1,46 @@
+/// Tests that DCS' GetIdFromArguments works as expected with standard and odd cases
+/datum/unit_test/dcs_get_id_from_arguments
+
+/datum/unit_test/dcs_get_id_from_arguments/Run()
+	assert_equal(list(1), list(1))
+	assert_equal(list(1, 2), list(1, 2))
+	assert_equal(list(src), list(src))
+
+	assert_equal(
+		list(a = "x", b = "y", c = "z"),
+		list(b = "y", a = "x", c = "z"),
+		list(c = "z", a = "x", b = "y"),
+	)
+
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list(1, 2)), get_id_from_arguments(list(2, 1)), "Swapped arguments should not return the same id")
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list(1, a = "x")), get_id_from_arguments(list(1)), "Named arguments were ignored when creating ids")
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list(1, a = "x")), get_id_from_arguments(list(a = "x")), "Unnamed arguments were ignored when creating ids")
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list(src)), get_id_from_arguments(list(world)), "References to different datums should not return the same id")
+
+	TEST_ASSERT_NOTEQUAL(get_id_from_arguments(list()), SSdcs.GetIdFromArguments(list(/datum/element/dcs_get_id_from_arguments_mock_element2)), "Different elements should not match the same id")
+
+/datum/unit_test/dcs_get_id_from_arguments/proc/assert_equal(reference, ...)
+	var/result = get_id_from_arguments(reference)
+
+	// Start at 1 so the 2nd argument is 2
+	var/index = 1
+
+	for (var/other_case in args)
+		index += 1
+
+		var/other_result = get_id_from_arguments(other_case)
+
+		if (other_result == result)
+			continue
+
+		Fail("Case #[index] produces a different GetIdFromArguments result from the first. [other_result] != [result]")
+
+/datum/unit_test/dcs_get_id_from_arguments/proc/get_id_from_arguments(list/arguments)
+	return SSdcs.GetIdFromArguments(list(/datum/element/dcs_get_id_from_arguments_mock_element) + arguments)
+
+// Necessary because GetIdFromArguments uses id_arg_index from an element type
+/datum/element/dcs_get_id_from_arguments_mock_element
+	id_arg_index = 2
+
+/datum/element/dcs_get_id_from_arguments_mock_element2
+	id_arg_index = 2